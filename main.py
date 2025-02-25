84bbb74fa278069301023aabdb00161313b5ee43
kevin-njuguna <kevy.kamah@gmail.com>
2025-02-25 22:35:02 +0300

    5. madlibs game

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-#INTRO
-name = input("what is your name: ")
-age= int(input("How old are you: "))
-age +=1
-print(f"Hello {name}")
-print(f"You are {age} years old")
+#madlibs game - word game where you create a story by filling in blanks with random words
+adjective1 = input("Enter an adjective (description): ")
+noun = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb (doing word):")
+adjective3 = input("Enter an adjective (description): ")
 
-#AREA OF RECTANGLE
-length = float(input("length (m): "))
-width = float(input("width (m): "))
-area = length *width
-
-print(f"length: {length} m")
-print(f"width: {width} m")
-print(f"area: {area} m²")
+print(f"Today I went to a {adjective1} zoo. ")
+print(f"In an exhibit, I saw a {noun}. ")
+print(f"{noun} was {adjective2} and {verb1}. ")
+print(f"I was {adjective3}!")

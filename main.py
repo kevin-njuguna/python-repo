38c0131cd6f2fe4fbcfd4f277bbfdba71ec6c10f
kevin-njuguna <kevy.kamah@gmail.com>
2025-02-25 23:25:35 +0300

    6.arithmetic & math

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
-#madlibs game - word game where you create a story by filling in blanks with random words
-adjective1 = input("Enter an adjective (description): ")
-noun = input("Enter a noun (person, place, thing): ")
-adjective2 = input("Enter an adjective (description): ")
-verb1 = input("Enter a verb (doing word):")
-adjective3 = input("Enter an adjective (description): ")
+import math
 
-print(f"Today I went to a {adjective1} zoo. ")
-print(f"In an exhibit, I saw a {noun}. ")
-print(f"{noun} was {adjective2} and {verb1}. ")
-print(f"I was {adjective3}!")
+friends = 10
+# (power) friends **= 2
+friends %= 2
+
+print(friends)
+
+x = 9.1
+y = 4
+z= 5 
+
+#result = round(x)
+#result = abs(y)
+#result = pow(y,3)
+result = max(x,y,z)
+
+
+
+#print(math.pi)
+#print(math.e)
+#result = math.sqrt(x)
+result = math.ceil(x)
+
+print(result)
+
+#Exercise 1: circumfrence of a circle
+radius = 2
+
+circumfrence = 2 * math.pi * radius
+print(round(circumfrence, 2))
+
+#Exercise 2: area of a circle
+radius = 2
+
+area = math.pi * (radius**2)
+print(round(area, 2))
+
+#exercise 3: hypotenuse
+a =3
+b=4
+
+c = math.sqrt(pow(a,2) + pow(b,2))
+print(c)
+
+

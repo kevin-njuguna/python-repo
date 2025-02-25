61468c595f09f06bb2243894235839987a768417
kevin-njuguna <kevy.kamah@gmail.com>
2025-02-25 11:50:02 +0300

    2. variables

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world!")
+#strings
+first_name = "Kevy"
+food = "Ugali"
+print(first_name)
+print(f"Hello {first_name}") #format specifier
+print(f"You like {food}")
+
+#integers
+age = 25
+print(f"You are {age} years old")
+
+#float
+price = 10.99
+print(f"The price is ${price}")
+
+#boolean
+is_student = True
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")

4242863b8e45f24e3439a81b421fc092eb977b7d
kevin-njuguna <kevy.kamah@gmail.com>
2025-02-27 10:13:43 +0300

    14. string indexing

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-""" name = input("Enter your name: ")
-result = len(name)
-
-print(result)
-position = name.find(" ")
-print(position)
-
-last_position = name.rfind("a")
-print(last_position)
-
-print(name.capitalize()) #capitalize the first letter of the first word
-print(name.upper())
-print(name.lower())
-print(name.isdigit())
-print(name.isalpha())
-
-"""
- 
-""" phone_number = input("Enter your phone number: ")
-space_length = phone_number.count(" ")
-new_phone_number = phone_number.replace(" ", "-")
-print(space_length)
-print(new_phone_number) """
-
-name = input("Enter your name: ")
-
-if len(name) >12:
-    print("Username is too long!")
-#elif not name.find(" ") == -1:
-    #print("Username must not contain spaces!")
-elif " " in name:
-    print("Username must not contain spaces!")
-elif name.isdigit():
-    print("Username must not contain digits")
-else:
-    print("success!")
+credit_number = "1234-5678-9012"
+print(credit_number[0])
+print(credit_number[0:4])
+print(credit_number[0:9:2])
+print(credit_number[5:])
+print(credit_number[-1])
+print(credit_number[-4:])
+#reverse a string
+print(credit_number[::-1])

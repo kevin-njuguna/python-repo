17cca186f4bc0f3bb036edf63694267ef88d3b16
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-01 16:22:03 +0300

    21. lists, sets and tuples

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
-rows = int(input("Enter number of rows: "))
-columns = int(input("Enter number of columns: "))
-symbol = (input("Enter a symbol: "))
+#1. LISTs 
 
-""" for x in range(3):
-    for x in range(1,10):
-        print(x, end="")
-    print() """
+""" fruits = ["apple", "orange", "banana"] 
+print(fruits[0])
+
     
-for x in range(rows):
-    for x in range(1, columns+1):
-        print(symbol, end="")
-    print()
+print(dir(fruits))
+print(help(fruits))
+print(len(fruits))
+print("apple" in fruits)
+fruits[0] = "watermelon"
+fruits.append("dragon fruit")
+fruits.remove("watermelon")
+fruits.insert(0, "watermelon")
+fruits.sort()
+fruits.reverse()
+fruits.clear()
+print(fruits.count("dragon fruit"))
+print(fruits.index("watermelon"))
+for fruit in fruits:
+    print(fruit) """
+
+#2. SET UNORDERED AND IMUTABLE. WE CAN HOWEVER ADD/REMOVE ELEMENTS.
+""" fruits = {"apple", "orange", "banana", "coconut"}
+
+print(dir(fruits))
+print(help(fruits))
+
+fruits.add("pineapple")
+fruits.remove("coconut")
+fruits.pop()
+fruits.clear()
+
+
+print(fruits) """
+
+#3. TUPLES. ORDERED AND UNCHANGEABLE. FASTER THAN LISTS
+
+fruits = ("apple", "pineapple", "orange", "banana")
+
+#print(dir(fruits))
+#print(help(fruits))
+#print(len(fruits))
+#print("pineapple" in fruits)
+
+print(fruits.index("apple"))
+print(fruits.count("orange"))
+print(fruits)
+for fruit in fruits:
+    print(fruit)

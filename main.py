afaf66b53cf066d770a0de6218781bfb301af844
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-01 19:31:33 +0300

    24. quiz game

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-""" groceries = [["apple", "orange", "banana", "coconut"],
-            ["celery", "carrots", "potatoes"],   
-            ["chicken", "fish", "turkey"]]
+questions = ("How many elements are in the periodic table?: ",
+             "Which animal lays the largest eggs?: ",
+             "What is the most abundant gas in the Earth's atmosphere?: ",
+             "How many bones are in the human body?: ",
+             "Which planet in the solar system is the hottest?: ")
 
-print(groceries[0][2])
+options = (("A.116","B. 117","C. 118","D. 119"),
+           ("A.Whale","B. Crocoldile","C. Elephant","D. Ostrich"),
+           ("A. Nitrogen","B. Oxygen","C. Carbon-Dioxide","D. Hydrogen"),
+           ("A. 206","B. 207","C. 208","D. 209"),
+           ("A. Mercury","B. Venus","C. Earth","D. Mars"))
 
-for collection in groceries:
-    for item in collection:
-        print(item, end=" ")
-    print() """
+answers = ("C","D","A","A","B")
+guesses = []
+score = 0
+question_num = 0
+
+for question in questions:
+    print("-------------------------------------------------------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
     
-phone_layout = [["1","2","3"],
-                ["4","5","6"],
-                ["7","8", "9"],
-                ["*", "0", "#"]]
+    guess = input("Enter (A, B, C, D): ").upper() 
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT!")
+    else:
+        print("INCORRECT!")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num +=1
+    
+print("-----------------------------")
+print("          RESULTS            ")
+print("-----------------------------")
 
-print(phone_layout)
-
-for collection in phone_layout:
-    for item in collection:
-        print(item, end=" ")
-    print()
+print("answers: ", end=" ")
+for answer in answers:
+    print(answer, end= " ")
+    
+print()
+        
+print("guesses: ", end=" ")
+for guess in guesses:
+    print(guess, end= " ")
+    
+print()
+score = score / len(questions) * 100
+print(f"Your score is: {score:.2f}%")

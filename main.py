a207b610257751a5edf4548f28edf378ca02c8aa
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-01 13:30:40 +0300

    18. for loops

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
-#compound interest calculator
-principle = 0
-rate = 0
-time = 0
-
-while principle <=0:
-    principle = float(input("Enter principle amount: "))
-    if principle <=0:
-        print("principle can't be less than or equal to 0")
-        
-while rate <=0:
-    rate = float(input("Enter interest rate: "))
-    if rate <=0:
-        print("rate can't be less than or equal to 0")
-        
-while True:
-    time = int(input("Enter time in years: "))
-    if time < 0:
-        print("time can't be less than 0")
-    else:
-        break
+#1 over a range
+""" for x in range(1, 11, 2):
+    print(x) """
     
-total = principle * pow((1 + rate/100), time)
-interest = total - principle
+#2 reversed
+""" for x in reversed(range(1, 11)):
+    print(x)
+print("HAPPY NEW YEAR!") """
 
-print(f"principle: {principle}")
-print(f"rate: {rate}")
-print(f"time: {time}")
+#3 over a string
+""" credit_card = "1234-5678-9012-3456"
 
-print(f"total amount: ${round(total, 2)}")
-print(f"interest: ${round(interest, 2)}")
+for x in credit_card:
+    print(x) """
+ 
+ #continue statement   
+for x in range(1,21):
+    if x==13:
+        continue
+    else:
+        print(x)

81d15332f57307e57889cc7ec378bf7420bb5dd5
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-20 11:37:20 +0300

    27. random numbers

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-#concession stand program
+import random
+#NUMBER OPTIONS
+#print(help(random))
+#number = random.randint(1,6)
+#number = random.random()
+#print(number)
 
-menu = {"ugali": 30.00,
-        "chapati": 20.00,
-        "samosa": 10.00,
-        "beans": 30.00,
-        "omena": 50.00}
+#STRING OPTIONS
+options = ("rock", "paper", "scissors")
+print(random.choice(options))
 
-cart = []
-total = 0
-
-print("-----------MENU-----------")
-for key, value in menu.items():
-    print(f"{key:10}: ${value:.2f}")
-    
-print("--------------------------")
-
-
-while True:
-    food = input("Select an item (q to quit): ").lower()
-    if food == "q":
-        break
-    elif menu.get(food) is not None:
-        cart.append(food)
-        
-#print(cart)
-print("--------YOUR ORDER--------")
-for food in cart:
-    total += menu.get(food)
-    print(food, end=" ")
-
-print()
-print(f"Total: ${total:.2f}")
-
-print("---------THANK YOU!---------")
+#SHUFFLING
+cards = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+random.shuffle(cards)
+print(cards)

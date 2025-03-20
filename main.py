2c556eab3d5c1e055325840cbaa30444879106db
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-20 16:17:35 +0300

    30. dice roller program

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 import random
 
-options = ("rock", "paper", "scissors")
-is_running = True
-play_again = ""
-  
-while is_running:
-    player = None
-    computer = random.choice(options)
+#● ┌ ─ ┐ │ └ ┘  
+
+"┌─────────┐"
+"│         │"
+"│         │"
+"│         │"
+"└─────────┘"
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│   ●     │",
+        "│         │",
+        "│       ● │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│ ●       │",
+        "│    ●    │",
+        "│       ● │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│ ●     ● │",
+        "│         │",
+        "│ ●     ● │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│ ●     ● │",
+        "│    ●    │",
+        "│ ●     ● │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "└─────────┘") 
+}
+
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?: "))
+
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1, 6))
     
-    while player not in options:
-        player = input("Enter a choice (rock, paper, scissors): ").lower()
-        print(f"Player: {player}")
-        print(f"Computer: {computer}")
+for die in range(num_of_dice):
+    for line in dice_art.get(dice[die]):
+        print(line)
+    
+for die in dice:
+    total+= die
+print(f"total: {total}")
 
-        if player == computer:
-            print("It's a tie!")
-        elif player == "rock" and computer == "scissors":
-            print("You win!")
-        elif player == "paper" and computer == "rock":
-            print("You win!")
-        elif player == "scissors" and computer == "paper":
-            print("You win!")
-            is_running = False
-        else:
-            print("You lose! Game over!")    
-            play_again == input("Play again (y/n): ").lower()
-            if not play_again=="y":
-                is_running = False
-
-print("Thanks for playing!")
-
-
+print(dice_art[5])

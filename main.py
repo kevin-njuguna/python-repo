1f06091be275fd6d77cbd87445621ada50947fea
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-21 12:30:03 +0300

    36. membership operators

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-#iterables - an object/collection that can return its elements one at a time, allowing it to be iterated voer in a loop
+#membership operators = used to test whether a variable is found in a sequence(string, list, tuple, set or dictionary)
+                #1.in
+                #2. not in
+                
+#EXAMPLE 1
+""" word = "apple"
 
-numbers = [1, 2, 3, 4]
+letter = input("Guess a letter in the secret word: ").lower()
 
-for number in reversed(numbers):
-    print(number, end= " ")
+if letter not in word:
+    print("Failed!")
+else:
+   print("CORRECT!")  """
+   
+
+#EXAMPLE 2
+grades = {
+    "Peter": "A",
+    "Fridah": "B",
+    "Maureen": "C",
+    "Moraa": "D"
+}
+
+student = input("Enter name of student: ").capitalize()
+
+if student in grades:
+    print(f"{student}'s grade is {grades[student]}")
+else:
+    print(f"{student} was not found!")

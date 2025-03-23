7b98b56a4f925879a96abffeee2a6c83077a6d4b
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-23 11:39:37 +0300

    39. modules

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-#EXAMPLE 1
-def day_of_week(day):
-    match day:
-        case 1 | 0:
-            return "It's Sunday"
-        case 2:
-            return "It's Monday"
-        case 3:
-            return "It's Tuesday"
-        case 4:
-            return "It's Wednesday"
-        case 5:
-            return "It's Thursday"
-        case 6:
-            return "It's Friday"
-        case 7:
-            return "It's Saturday"
-        case _:
-            return("Not a valid day!")
-        
-print(day_of_week(0))
+#print(help("modules"))
+#print(help("math"))
+
+import math
+import example
+#import math as m
+#from math import pi
+print(math.pi)
+
+result = example.pi
+print(result)
+print(example.square(3))
+print(example.circumfrence(4))

fe21087c07b0c3d06ba93e44ec928aa8ffe8dea3
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-25 00:45:57 +0300

    47. class variables

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from car import *
+#class variables - shared among all instances of a class
+# defined outside the constructor
+# allow you to share data among all objects created from that class
 
+class Student:
+    
+   class_year = 2030
+   num_students = 0
+    
+   def __init__(self, name, age):
+      self.name = name
+      self.age = age
+      Student.num_students += 1
+      
+student1 = Student("Maureen",  19)
+student2 = Student("Fridah",  20)
 
-car1.drive()
+print(student1.name)
+#print(student2.class_year)
+print(Student.class_year)
+print(Student.num_students)

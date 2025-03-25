6d51bb0552f56de980969456c35f5b124eb26a12
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-25 20:38:35 +0300

    54. class methods

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-class Employee:
-    def __init__(self, name, position):
-        self.name = name
-        self.position = position
-        
-    def get_info(self):#instance method
-        return f"{self.name} = {self.position}"
-    
-    @staticmethod
-    def is_valid_position(position):
-        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
-        return position in valid_positions
-    
-employee1 = Employee("Eugene", "Manager")
-employee2 = Employee("Squidward", "Cashier")
-employee3 = Employee("Spongebob", "Cook")
+# class methods - allow operations related to the class itself
+#Take (cls) as the first parameter, which represents the class itself
 
-print(Employee.is_valid_position("Cook"))
-print(employee1.get_info())
+class Student:
+    count = 0
+    total_gpa = 0
+    
+    def __init__(self,name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Student.count += 1 #Whenever we create a student object, increase count by 1
+        Student.total_gpa += gpa
+     
+    #INSTANCE METHOD   
+    def get_info(self):
+        return f"{self.name} : {self.gpa}"
+    
+    @classmethod
+    def get_count(cls):
+        return f"Total number of students: {cls.count}"
+    
+    
+    @classmethod
+    def get_average_gpa(cls):
+        if cls.count == 0:
+            return 0
+        else:
+            return f"total gpa: {cls.total_gpa / cls.count}"
+
+student1 = Student("Spongebob", 2)
+student1 = Student("Patrick", 4)
+student1 = Student("Patrick", 3)
+
+print(Student.get_count())
+print(Student.get_average_gpa())

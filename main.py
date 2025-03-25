717106dd207428d0b5a4b395ba4849f26d02bc38
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-25 18:32:01 +0300

    52. duck typing

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-from abc import ABC, abstractmethod
-
-class Shape:
+class Animal:
+    alive = True
     
-    @abstractmethod
-    def area(self):
-        pass
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-      
-    def area(self):
-        return 3.142 * self.radius ** 2
-
-class Square(Shape):
-    def __init__(self, side):
-        self.side = side
-
-    def area(self):
-        return  self.side ** 2
-class Triangle(Shape):
-    def __init__(self, base, height):
-        self.base = base
-        self.height = height
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
         
-    def area(self):
-        return self.height * self.base ** 0.5
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+        
+class Car(Animal):
+    alive = False
+    def speak(self):
+        print("HONK!")
+        
+animals = [Dog(), Cat(), Car()]
 
-shapes = [Circle(4), Square(4), Triangle(3, 4)]
-
-for shape in shapes:
-    print(f"{shape.area():.2f} cm²" )
+for animal in animals:
+    animal.speak()
+    print(animal.alive)

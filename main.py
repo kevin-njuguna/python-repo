726443f7013648b1d2f517437c047dc6a27eda1e
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-27 09:45:58 +0300

    58. exception handling

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-#decorator - a function that extends the behaviour of another funtion w/o modifying the base function
-#pass the base function as an argument to the decorator
-
-#decor ator
-def add_sprinkles(func):
-    def wrapper(*args, **kwargs):
-        print("***You add sprinkles🎊")
-        func(*args, **kwargs)
-    return wrapper
-
-def add_fudge(func):
-    def wrapper(*args, **kwargs):
-        print("You add fudge")
-        func(*args, **kwargs)
-    return wrapper
-        
-@add_sprinkles
-@add_fudge 
-def get_ice_cream(flavor):
-    print(f"Here is your {flavor} ice cream🍨")
-    
-
-get_ice_cream("vanilla")
-
+try:
+    number = int(input("Enter a number: "))
+    print(1 / number)
+except ZeroDivisionError:
+    print("You can't divide by zero")
+except ValueError:
+    print("Enter only numbers please")
+except Exception:
+    print("Something went wrong!")
+finally:
+    print("Do some cleanup here!")

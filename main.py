904136de2658241db9d6213c75f34d413cdb6895
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-27 13:39:32 +0300

    61. reading files

--- main.py
+++ main.py
@@ -1,55 +1,45 @@
-# .TXT
-""" employees = ["Eugene", "Squidward", "Spongebob", "Patrick"]
-txt_data = "I like pizza"
-
-file_path = "output.txt"
+#.TXT
+""" file_path ="output.txt"
 
 try:
-    with open(file_path, "w") as file:
-        for employee in employees:
-            file.write(employee + "\n")
-        print(f"txt file '{file_path}' was created")
-except FileExistsError:
-    print("file exists")
+    with open(file_path, "r") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print("That file not found")
+except PermissionError:
+    print("You do not have permission to read that file") """
     
- """
- 
- #JSON
+
+#JSON
 """ import json
- 
-employees = {
-    "name": "Spongebob",
-    "age": 30,
-    "job": "cook"
-}
 
 file_path = "output.json"
 
 try:
-    with open(file_path, "w") as file:
-        json.dump(employees, file, indent=4)
-        print(f"json file '{file_path}' was created")
-except FileExistsError:
-    print("file exists")
-     """
+    with open(file_path, "r") as file:
+        content = json.load(file)
+        print(content)
+        print(content["name"])
+except FileNotFoundError:
+    print("That file not found")
+except PermissionError:
+    print("You do not have permission to read that file") """
+    
 
 #CSV
-import json
 import csv
- 
-employees = [["Name", "Age", "Job"],
-             ["Sandy", 27, "Scientist"],
-             ["Spongebob", 30, "Cook"],
-             ["Patrick", 37, "Unemployed"]]
 
 file_path = "output.csv"
 
 try:
-    with open(file_path, "w", newline="") as file:
-        writer = csv.writer(file)
-        for row in employees:
-            writer.writerow(row)
-        print(f"csv file 'file_path' was created")
-except FileExistsError:
-    print("file exists")
-    
+    with open(file_path, "r") as file:
+        content = csv.reader(file)
+        for line in content:
+            print(line)
+            print(line[2])
+        
+except FileNotFoundError:
+    print("That file not found")
+except PermissionError:
+    print("You do not have permission to read that file")

351734a8bbdf08c203608e05e66c57ff0054fcb9
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-27 15:26:39 +0300

    63. alarm clock

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-import datetime
+import time
+import datetime 
 
-date = datetime.date(2025, 1, 2)
-today = datetime.date.today()
+import pygame
 
-time = datetime.time(12, 30, 0)
-now = datetime.datetime.now()
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "Deck The Halls - The Soundlings.mp3"
+    is_running = True
+    
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        
+        if current_time == alarm_time:
+            print("Wake up!")
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+            
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+            is_running = False
+        
+        time.sleep(1)
+        
+        
 
-now = now.strftime("%H:%M:%S %d-%m-%Y")
-
-target_date_time = datetime.datetime(2040, 1, 2,  12, 30, 1)
-current_date_time = datetime.datetime.now()
-
-#print(date)
-#print(today)
-#print(now)
-
-if target_date_time < current_date_time:
-    print("Target Date has passed")
-else:
-    print("Target date has not passed")
-
-
+if __name__ == "__main__":
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)

9e50a72dd28cb3aba3b51c3062cc764c13ab9e7e
kevin-njuguna <kevy.kamah@gmail.com>
2025-03-27 12:24:39 +0300

    60. writing files

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
-import os
+# .TXT
+""" employees = ["Eugene", "Squidward", "Spongebob", "Patrick"]
+txt_data = "I like pizza"
 
-file_path = "C:\\Users\\Admin\\Desktop\\test"
+file_path = "output.txt"
 
-if os.path.exists(file_path):
-    print(f"The location  '{file_path}' exists")
+try:
+    with open(file_path, "w") as file:
+        for employee in employees:
+            file.write(employee + "\n")
+        print(f"txt file '{file_path}' was created")
+except FileExistsError:
+    print("file exists")
     
-    if os.path.isfile(file_path):
-        print("That is a file")
-    elif os.path.isdir(file_path):
-        print("That is a folder")
-else:
-    print("The location doesn't exist")
-        
+ """
+ 
+ #JSON
+""" import json
+ 
+employees = {
+    "name": "Spongebob",
+    "age": 30,
+    "job": "cook"
+}
+
+file_path = "output.json"
+
+try:
+    with open(file_path, "w") as file:
+        json.dump(employees, file, indent=4)
+        print(f"json file '{file_path}' was created")
+except FileExistsError:
+    print("file exists")
+     """
+
+#CSV
+import json
+import csv
+ 
+employees = [["Name", "Age", "Job"],
+             ["Sandy", 27, "Scientist"],
+             ["Spongebob", 30, "Cook"],
+             ["Patrick", 37, "Unemployed"]]
+
+file_path = "output.csv"
+
+try:
+    with open(file_path, "w", newline="") as file:
+        writer = csv.writer(file)
+        for row in employees:
+            writer.writerow(row)
+        print(f"csv file 'file_path' was created")
+except FileExistsError:
+    print("file exists")
+    
